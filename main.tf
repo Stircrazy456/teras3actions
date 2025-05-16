9d41eb2db3f20de100c50d8d9f4e48621b8c4636
Stircrazy456 <rayabagipavan@gmail.com>
2025-05-17 01:01:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-  region = var.aws_region
-  access_key              = secrets.AWS_ACCESS_KEY_ID
-  secret_key              = secrets.AWS_SECRET_ACCESS_KEY
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
+
 
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
