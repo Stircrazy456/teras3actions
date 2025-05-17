3e0133178d7d2bae35fba0c041fdba31374d5aa7
Stircrazy456 <rayabagipavan@gmail.com>
2025-05-17 05:41:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,23 @@
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-variable "aws_access_key" {
-  type = string
+  description = "AWS region"
+  default     = "us-east-1"
 }
 
-variable "aws_secret_key" {
-  type = string
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
 }
 
+variable "key_name" {
+  description = "Name of the existing EC2 key pair"
+  default = "2023"
+}
 
-variable "bucket_name" {
-  type = string
-  default = "wertyuiolmnbvcxdf678"
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket for remote state"
+  default = "tfdemo16112023"
+}
+
+variable "name" {
+  description = "Name of EC2 Intance"
 }
