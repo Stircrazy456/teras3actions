c9111a77630415df3e64369ba93e103dec67fdae
Stircrazy456 <rayabagipavan@gmail.com>
2025-05-17 05:38:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "s306022024"
+    bucket         = "s306022024yvg"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
