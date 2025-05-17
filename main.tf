7c104da2e189fdd8628fdd5c16792942d57bc8e3
Stircrazy456 <rayabagipavan@gmail.com>
2025-05-17 07:52:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+#resource "aws_s3_bucket" "example" {
+#  bucket = var.bucket_name
 
+#}
+
+resource "aws_s3_bucket" "example" {
+  bucket        = var.bucket_name
+  force_destroy = true  # Allows Terraform to delete the bucket even if it contains objects
 }
+
 
 #provider "aws" {
 #  region = var.aws_region
